c64033bca0eef69919c34e91639ed0e032422dd6
fakegyptian <68777275+fakegyptian@users.noreply.github.com>
2023-01-09 10:31:39 +0300

    Add files via upload

---
+++ IF ELSE/IF ELSE/IF ELSE/Program.cs
@@ -0,0 +1,21 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if (time > 18)
+            System.Console.WriteLine("İyi akşamlar");
+        else if (time > 12)
+            System.Console.WriteLine("İyi günler");
+        else
+        {
+            System.Console.WriteLine("İyi sabahlar");
+        }
+
+        //ternary if
+        string mesaj = time > 18 ? "iyi geceler" : "iyi günler";
+        System.Console.WriteLine(mesaj);
+    }
+}
